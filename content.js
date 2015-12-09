b8a0414fc2892e2bb9e238da12dfdb8bcead60e7
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2015-12-09 11:20:23 -0800

    added top nav bar, left side column, and status update DOM IDs

---
+++ content.js
@@ -0,0 +1,5 @@
+var facebook_Nav_Bar = document.getElementById('pagelet_bluebar');
+
+var facebook_Left_Side_Column = document.getElementById('leftCol');
+
+var status_Update_Area = document.getElementById('pagelet_composer');
