effb59f3cf9a83747c7015845adda7774376be48
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2015-12-09 16:28:53 -0800

    updates
    * ‘search facebook’ placeholder text: jedibook/sithbook
    * status update placeholder text
    * replace images of status updates

--- jedi.js
+++ jedi.js
@@ -1,10 +1,13 @@
+var placehold = document.getElementsByName('q')[0];
+placehold.placeholder = 'Search Jedibook';
+
 var elements = document.getElementsByTagName('*');
 var fb_nav_class = document.getElementById('blueBarNAXAnchor');
 for (var i = 0; i < elements.length; i++) {
    var element = elements[i];
    for (var j = 0; j < element.childNodes.length; j++) {
       var node = element.childNodes[j];
-      console.log(element.value)
+      //console.log(element.value)
       if (node.nodeType === 3) {
          var text = node.nodeValue;
          var replacedText = text.replace(/facebook/gi, "Jedibook");
@@ -17,8 +20,22 @@ for (var i = 0; i < elements.length; i++) {
 fb_nav_class.style.backgroundImage = "linear-gradient(rgba(55,132,214,1) 0%,rgba(254,254,254,1) 30%,rgba(254,254,254,1) 47%,rgba(254,254,254,1) 71%,rgba(55,132,214,1) 100%)"
 
 // "What's on your mind"
-var status_Text_Placeholder = document.getElementById('u_0_y')[0];
+var status_Text_Placeholder = document.getElementById('u_0_y');
 status_Text_Placeholder.placeholder = "Try not. Do… or do not. There is no try.";
+
+
+//targets all images on the main page
+var arrayOfImageTags = document.getElementsByClassName('_38vo');
+for (var key in arrayOfImageTags) {
+
+   if (arrayOfImageTags[0]) {
+      arrayOfImageTags[key].firstChild.src = 'http://cdn.bgr.com/2015/08/darth-vader.jpg'
+   } else {
+      arrayOfImageTags[key].firstChild.src = 'http://webmaster.ypsa.org/wp-content/uploads/2012/08/no_thumb.jpg'
+   }
+}
+
+
 
 
 // };
--- sith.js
+++ sith.js
@@ -1,21 +1,31 @@
+var placehold = document.getElementsByName('q')[0];
+placehold.placeholder = 'Search Sithbook';
+
+
 var elements = document.getElementsByTagName('*');
 var fb_nav_class = document.getElementById('blueBarNAXAnchor');
 for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-        console.log(element.value)
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/jedibook/gi, "Sithbook");
+   var element = elements[i];
+   for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+      //console.log(element.value)
+      if (node.nodeType === 3) {
+         var text = node.nodeValue;
+         var replacedText = text.replace(/jedibook/gi, "Sithbook");
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
+         if (replacedText !== text) {
+            element.replaceChild(document.createTextNode(replacedText), node);
+         }
+      }
+   }
 }
 fb_nav_class.style.backgroundImage = "linear-gradient(rgba(229,17,21,1) 0%,rgba(0,0,0,1) 30%,rgba(0,0,0,1) 47%,rgba(0,0,0,1) 71%,rgba(229,17,21,1) 100%)";
+
+
+// "What's on your mind"
+var status_Text_Placeholder = document.getElementById('u_0_y');
+status_Text_Placeholder.placeholder = "Good! Use your aggressive feelings, boy. Let the hate flow through you! ";
+
 
 // };
 // changeFacebookText("theMe")
